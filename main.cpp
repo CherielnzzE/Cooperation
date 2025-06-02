5e98a747384d9b78183325d574b5bc80c0faa257
sc-77 <sharon940717@gmail.com>
2025-06-02 16:44:25 +0800

    feat: add bar() and call it in main

--- main.cpp
+++ main.cpp
@@ -5,10 +5,13 @@ void foo() {
     cout << "It’s foo!!" << endl;
 }
 
-void bar() {}
+void bar() {
+    cout<<"It's bar!"<<endl;
+}
 
 int main() {
     cout << "Hello, World!" << endl;
     foo();  // 呼叫 foo
+    bar();  //呼叫 bar
     return 0;
 }
