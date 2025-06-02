2092b5f6b8935a0d700914563fe410dffb34b990
sc-77 <sharon940717@gmail.com>
2025-06-02 17:30:43 +0800

    ft: bar

--- main.cpp
+++ main.cpp
@@ -6,12 +6,12 @@ void foo() {
 }
 
 void bar() {
-
-    
+    cout<<"It's bar!"<<endl;
 }
 
 int main() {
     cout << "Hello, World!" << endl;
     foo();  // 呼叫 foo
+    bar();  //呼叫bar
     return 0;
 }
